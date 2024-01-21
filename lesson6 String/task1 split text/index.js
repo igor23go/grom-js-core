d8cdf4a8d9c1f08caec13464be04d243a55004e3
igor23go <Goigorit23@gmail.com>
2024-01-21 17:31:41 +0200

    split text

---
+++ lesson6 String/task1 split text/index.js
@@ -0,0 +1,17 @@
+function splitText(str, num = 10) {
+  const array = [];
+  let position = 0;
+  if (!str) return null;
+  while (true) {
+    const chank = str.substr(position, num);
+    if (chank.length === 0) {
+      break;
+    }
+    array.push(chank[0].toUpperCase().concat(chank.slice(1)));
+    position += num;
+  }
+  return array.join('\n');
+}
+
+const abc = 'abcdefghigklmnopqrstuvwxyz';
+console.log(splitText(abc, 4));
